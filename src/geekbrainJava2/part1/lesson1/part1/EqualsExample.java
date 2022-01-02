e30bdee8a25fbe555079150b0f116f96630bd9c1
DMITRIYAVDOTCHENKOV <dimka.a2010@yandex.ru>
2022-01-02 23:49:18 +0300

    Lesson1_2_3_Java2

---
+++ src/geekbrainJava2/part1/lesson1/part1/EqualsExample.java
@@ -0,0 +1,12 @@
+package geekbrainJava2.part1.lesson1.part1;
+
+public class EqualsExample {
+    public static void main(String[] args) {
+
+    String s3 = "Kotlin";
+    String s4 = "Kotlin";
+
+        System.out.println(s3 == s4);
+        System.out.println("Kotlin is equals " + (s3==s4));
+}
+}
---
+++ src/geekbrainJava2/part1/lesson1/part3/Pet.java
@@ -0,0 +1,9 @@
+package geekbrainJava2.part1.lesson1.part3;
+
+public interface Pet {
+
+    void loveMaster();
+
+    boolean isUseful();
+
+}
