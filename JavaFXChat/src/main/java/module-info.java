ac0edaa7cb47b4c8c1e9517e6e12a0a5790c1c3a
DMITRIYAVDOTCHENKOV <dimka.a2010@yandex.ru>
2022-01-20 00:32:08 +0300

    Lesson6_dz6_FINAL

--- JavaFXChat/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.javafxchat {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires javafx.graphics;
-
-
-    opens com.example.javafxchat to javafx.fxml;
-    exports com.example.javafxchat;
-}
